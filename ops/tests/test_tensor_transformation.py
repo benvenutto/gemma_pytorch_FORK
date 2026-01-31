992ec08cb7309bbb2aa275ca9eb21c81f99bda84
Simon Benvenutto <simon.benvenutto@gmail.com>
2026-01-31 17:05:56 +0000

    >>> convert/predict works, however index_copy_() is not getting invoked, as I've swapped it for a simple multiply (so the composite operator not in play)

--- ops/tests/test_tensor_transformation.py
+++ ops/tests/test_tensor_transformation.py
@@ -1,7 +1,11 @@
 from typing import Final
+
 import torch
 from torch import nn
 from torch.export import Dim
+
+import coremltools as ct
+import numpy as np
 
 from .test_util import torch_export_model, coreml_convert_model, run_coreml_model
 
@@ -11,11 +15,20 @@ class TestTensorIndexCopySlice(nn.Module):
 
     def __init__(self, target):
         super().__init__()
-        self.register_buffer('target', target)
+        self.register_buffer('target', target, persistent=False)
+
+    # def forward(self, slice_indices: torch.Tensor, slice_values: torch.Tensor) -> torch.Tensor:
+    #     self.target.index_copy_(TestTensorIndexCopySlice.SEQUENCE_DIM, slice_indices, slice_values)
+    #     return self.target
 
     def forward(self, slice_indices: torch.Tensor, slice_values: torch.Tensor) -> torch.Tensor:
-        self.target.index_copy_(TestTensorIndexCopySlice.SEQUENCE_DIM, slice_indices, slice_values)
+        # slice_index = slice_indices[0]
+        # slice_value = slice_values[:, slice_index]
+        slice_value = slice_values[:, 0]
+        slice_value =  slice_value.unsqueeze(TestTensorIndexCopySlice.SEQUENCE_DIM)
+        self.target = self.target * slice_value     ### with broadcast
         return self.target
+
 
 def test_index_copy():
 
@@ -25,19 +38,21 @@ def test_index_copy():
 
     torch_device = torch.device('mps')
     torch_data_dtype = torch.float32
+    torch_state_dtype = torch.float16
     torch_index_dtype = torch.int64
+    torch_coreml_int = torch.int32
 
-    target = torch.rand((8, 24, 6, 4), dtype=torch_data_dtype, device=torch_device)
-    torch_model = TestTensorIndexCopySlice(target).to(device=torch_device, dtype=torch_data_dtype)
+    target = torch.rand((8, 24, 6, 4), dtype=torch_state_dtype, device=torch_device)
+    torch_model = TestTensorIndexCopySlice(target).to(device=torch_device)
 
-    original_target = target.clone()
+    # original_target = target.clone()
     slice_indices = torch.tensor([2, 3, 5], dtype=torch_index_dtype, device=torch_device)
-    original_target_slice = original_target[:, slice_indices]   ### dim=1
+    # original_target_slice = original_target[:, slice_indices]   ### dim=1
     slice_values = torch.rand((8, 3, 6, 4), dtype=torch_data_dtype, device=torch_device)
 
     ### Update with the slice
     updated_target = torch_model(slice_indices, slice_values)
-    assert not torch.equal(updated_target, original_target_slice)
+    # assert not torch.equal(updated_target, original_target_slice)
 
     # ### Update with the original values that were in the slice
     # restored_target = torch_model(slice_indices, original_target_slice)
@@ -51,7 +66,7 @@ def test_index_copy():
         slice_indices,
         slice_values,
     )
-    batch_size_dim = Dim("batch_size", min=1, max=256)
+    batch_size_dim = Dim.AUTO(min=1)
     indices_dim = Dim("indices", min=1)
     dynamic_shapes = {
         'slice_indices': (indices_dim, ),
@@ -63,17 +78,32 @@ def test_index_copy():
     ### Convert the CoreML model
     ###
 
-    coreml_model = coreml_convert_model(aten_program, )
+    state_desc = [
+        ct.StateType(
+            wrapped_type=ct.TensorType(
+                shape=target.shape,
+                dtype=np.float16,
+            ),
+            name='target',
+        ),
+    ]
+    coreml_model = coreml_convert_model(aten_program,
+                                        states=state_desc,
+                                        )
 
     ###
     ### Run the CoreML model to replicate PyTorch inference
     ###
 
     coreml_inputs = {
-        'slice_values': slice_values,
-        'slice_indices': slice_indices,
+        'slice_indices': slice_indices.cpu().to(dtype=torch_coreml_int).numpy(),
+        'slice_values': slice_values.cpu().to(dtype=torch_data_dtype).numpy(),
     }
-    coreml_updated_target = run_coreml_model(coreml_model, coreml_inputs)
+    target_state = target.cpu().to(dtype=torch_data_dtype).numpy()  ### Bah! use float32 not float16 ?!?
+    coreml_updated_target = run_coreml_model(
+        coreml_model,
+        coreml_inputs,
+        state_kv=('target', target_state),
+    )
 
     print(coreml_updated_target)
-
--- ops/tests/test_util.py
+++ ops/tests/test_util.py
@@ -1,6 +1,9 @@
-import copy
+from typing import Tuple
 import torch
+
 import coremltools as ct
+from coremltools.models.model import MLState
+
 import numpy as np
 
 
@@ -15,25 +18,24 @@ def torch_export_model(torch_model, model_inputs, dynamic_shapes):
     return simplified_aten_program
 
 
-def coreml_convert_model(aten_program):
+def coreml_convert_model(aten_program, states=None):
     coreml_model = ct.convert(
         aten_program,
         source='pytorch',
         convert_to='mlprogram',
         minimum_deployment_target=ct.target.iOS18,
         compute_units=ct.ComputeUnit.ALL,
-        # states=[
-        #         ct.StateType(
-        #             wrapped_type=ct.TensorType(
-        #                 # shape=kv_cache_size,
-        #                 dtype=np.float16,
-        #             ),
-        #             name='k_caches',
-        #         ),
-        # ],
+        states=states,
     )
     return coreml_model
 
 
-def run_coreml_model(coreml_model, coreml_inputs):
-    coreml_model.predict(coreml_inputs)
+def run_coreml_model(coreml_model, coreml_inputs, state_kv=None|Tuple[str, np._typing.NDArray[np.float16]]):
+    if state_kv is not None:
+        model_state: MLState = coreml_model.make_state()
+        model_state.write_state(name=state_kv[0], value=state_kv[1])
+    else:
+        model_state: MLState = None
+
+    preds = coreml_model.predict(coreml_inputs, state=model_state)
+    return preds
